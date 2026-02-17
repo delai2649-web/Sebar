161c8400511235218dc702d54d6404749ad3dcf4
delai2649-web <delai2649@gmail.com>
2026-02-17 19:18:53 +0700

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,4 +1,2 @@
-# Plugins package
-from .broadcast import BroadcastManager, broadcast_callback_handler
-from .groups import GroupManager, group_callback_handler
-from .scheduler import BroadcastScheduler
+from .userbot_generator import userbot_gen
+from .broadcast import BroadcastManager
