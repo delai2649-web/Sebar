af057bbe806a4d0043715b34f396f67f9fdb068e
delai2649-web <delai2649@gmail.com>
2026-02-17 18:09:33 +0700

    Initialize plugins package with imports

---
+++ plugins/__init__.py
@@ -0,0 +1,4 @@
+# Plugins package
+from .broadcast import BroadcastManager, broadcast_callback_handler
+from .groups import GroupManager, group_callback_handler
+from .scheduler import BroadcastScheduler
